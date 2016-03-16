ec70ecb328cce2406f398035266db09c7f419f5b
Totiimon <tomasdv2@gmail.com>
2016-03-16 15:07:53 -0300

    Added an Android controller to have magic1311.com/android so the people buying the app from the store have a place to go and know how to register as an user to later activate the purchased software.
    Added a view to the store controller that is where the paypal stuff will be in.
    
    changed some stuff in the navbar and the shared layout style
    
    Changes in CSS

---
+++ REST_magic1311/Controllers/AndroidController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace REST_magic1311.Controllers
+{
+    public class AndroidController : Controller
+    {
+        // GET: Android
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
